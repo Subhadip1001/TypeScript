ab9d5cdc848f7f321c275b765cf34d86fe90f72e
Subhadip Mandal <subhadipmandal714@gmail.com>
2025-06-18 10:28:59 +0530

    Types of ts

---
+++ dist/variables.js
@@ -0,0 +1,6 @@
+"use strict";
+let Name = 'Subhadip Mandal';
+let Age = 22;
+let Height = 5.6;
+let IsStudent = true;
+let randomValue = 'Hello, World!';
---
+++ src/variables.ts
@@ -0,0 +1,14 @@
+// string
+let Name: string = 'Subhadip Mandal';
+
+// number
+let Age: number = 22;
+let Height: number = 5.6;
+
+// boolean
+let IsStudent: boolean = true;
+
+// any
+let randomValue: any = 'Hello, World!';
+// randomValue = 42; // can be reassigned to any type
+// randomValue = true; // can also be reassigned to boolean
