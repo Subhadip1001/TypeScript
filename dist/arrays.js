e5d8567dea6c9a954f4abf00c06187c4a708dcbc
Subhadip Mandal <subhadipmandal714@gmail.com>
2025-06-25 20:15:00 +0530

    Array

---
+++ dist/arrays.js
@@ -0,0 +1,11 @@
+"use strict";
+const superHeroes = ["Ironman", "Spiderman", "Hulk", "Thor",];
+const numbers = [1, 2, 3, 4, 5];
+const users = [
+    { Name: "Subhadip", Age: 22, id: 1 },
+    { Name: "John", Age: 25, id: 2 },
+    { Name: "Jane", Age: 28, id: 3 }
+];
+const twoD = [];
+twoD.push([1, 2, 3]);
+twoD.push([4, 5, 6]);
---
+++ src/arrays.ts
@@ -0,0 +1,20 @@
+const superHeroes: string[] = ["Ironman","Spiderman","Hulk","Thor",];
+
+const numbers: Array<number> = [1, 2, 3, 4, 5];
+
+type allUser = {
+    Name: string;
+    Age: number;
+    id: number;
+}
+
+const users: allUser[] = [
+    { Name: "Subhadip", Age: 22, id: 1 },
+    { Name: "John", Age: 25, id: 2 },
+    { Name: "Jane", Age: 28, id: 3 }
+];
+
+const twoD: number[][] = []
+
+twoD.push([1, 2, 3]);
+twoD.push([4, 5, 6]);
